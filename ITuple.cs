f2eb0eb83e2a61bb874f862df876ec9d765be2a8
seeseekey <kontakt@seeseekey.net>
2015-07-09 18:30:49 +0200

    Add comments on public methods and properties.

--- ITuple.cs
+++ ITuple.cs
@@ -7,11 +7,34 @@ namespace Xevle.Math
 	/// </summary>
     public interface ITuple
     {
+		/// <summary>
+		/// Gets or sets the value U.
+		/// </summary>
+		/// <value>U</value>
         double U { get; set; }
+
+		/// <summary>
+		/// Gets or sets the value V.
+		/// </summary>
+		/// <value>The v.</value>
         double V { get; set; }
 
+		/// <summary>
+		/// Gets or sets the value X.
+		/// </summary>
+		/// <value>The x.</value>
         double X { get; set; }
+
+		/// <summary>
+		/// Gets or sets the value Y.
+		/// </summary>
+		/// <value>The y.</value>
         double Y { get; set; }
+
+		/// <summary>
+		/// Gets or sets the value Z.
+		/// </summary>
+		/// <value>The z.</value>
         double Z { get; set; }
     }
 }
--- Tuple3dc.cs
+++ Tuple3dc.cs
@@ -8,12 +8,27 @@ namespace Xevle.Math
     public class Tuple3dc: ITuple
     {
         #region Variables
+		/// <summary>
+		/// Variable to store the x value.
+		/// </summary>
         public double x;
+
+		/// <summary>
+		/// Variable to store the y value.
+		/// </summary>
         public double y;
+
+		/// <summary>
+		/// Variable to store the z value.
+		/// </summary>
         public double z;
         #endregion
 
         #region Constructors
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Xevle.Math.Tuple3dc"/> class.
+		/// </summary>
+		/// <param name="tuple">Instance of an ITuple implementor.</param>
         public Tuple3dc(ITuple tuple)
         {
             x=tuple.X;
@@ -21,6 +36,12 @@ namespace Xevle.Math
             z=tuple.Z;
         }
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Xevle.Math.Tuple3dc"/> class.
+		/// </summary>
+		/// <param name="x">The x coordinate.</param>
+		/// <param name="y">The y coordinate.</param>
+		/// <param name="z">The z coordinate.</param>
         public Tuple3dc(double x=0, double y=0, double z=0)
         {
             this.x=x;
@@ -37,6 +58,8 @@ namespace Xevle.Math
 			return new Tuple3dc (a.x + b.x, a.y + b.y, a.z + b.z);
 		}
 
+		/// <param name="a">The first Tuple</param>
+		/// <param name="b">The second Tuple</param>
 		public static Tuple3dc operator- (Tuple3dc a, Tuple3dc b)
 		{
 			return new Tuple3dc (a.x - b.x, a.y - b.y, a.z - b.z);
@@ -44,6 +67,10 @@ namespace Xevle.Math
 		#endregion
 
         #region ITuple implementation
+		/// <summary>
+		/// Gets or sets the value U.
+		/// </summary>
+		/// <value>U</value>
         public double U
         {
             get
@@ -55,6 +82,10 @@ namespace Xevle.Math
             }
         }
 
+		/// <summary>
+		/// Gets or sets the value V.
+		/// </summary>
+		/// <value>The v.</value>
         public double V
         {
             get
@@ -66,6 +97,10 @@ namespace Xevle.Math
             }
         }
 
+		/// <summary>
+		/// Gets or sets the value X.
+		/// </summary>
+		/// <value>The x.</value>
         public double X
         {
             get
@@ -78,6 +113,10 @@ namespace Xevle.Math
             }
         }
 
+		/// <summary>
+		/// Gets or sets the value Y.
+		/// </summary>
+		/// <value>The y.</value>
         public double Y
         {
             get
@@ -90,6 +129,10 @@ namespace Xevle.Math
             }
         }
 
+		/// <summary>
+		/// Gets or sets the value Z.
+		/// </summary>
+		/// <value>The z.</value>
         public double Z
         {
             get
