d3a35480849ffe9217303ac1bc6cf3d660c56ca9
seeseekey <kontakt@seeseekey.net>
2015-07-20 19:05:38 +0200

    Implement ITupleExtensions extension class.

--- Tuples/ITuple.cs
+++ Tuples/ITuple.cs
@@ -7,36 +7,36 @@ namespace Xevle.Math.Tuples
 	/// Interface for all Tuple classes and structs
 	/// </summary>
 	public interface ITuple: ICopy<ITuple>
-    {
+	{
 		/// <summary>
 		/// Gets or sets the value U.
 		/// </summary>
 		/// <value>U</value>
-        double U { get; set; }
+		double U { get; set; }
 
 		/// <summary>
 		/// Gets or sets the value V.
 		/// </summary>
 		/// <value>The v.</value>
-        double V { get; set; }
+		double V { get; set; }
 
 		/// <summary>
 		/// Gets or sets the value X.
 		/// </summary>
 		/// <value>The x.</value>
-        double X { get; set; }
+		double X { get; set; }
 
 		/// <summary>
 		/// Gets or sets the value Y.
 		/// </summary>
 		/// <value>The y.</value>
-        double Y { get; set; }
+		double Y { get; set; }
 
 		/// <summary>
 		/// Gets or sets the value Z.
 		/// </summary>
 		/// <value>The z.</value>
-        double Z { get; set; }
-    }
+		double Z { get; set; }
+	}
 }
 
---
+++ Tuples/ITupleExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xevle.Math.Tuples;
+
+namespace Xevle.Math.Tuples
+{
+	/// <summary>
+	/// Static class that server ITuple extensions methods.
+	/// </summary>
+	public static class ITupleExtensions
+	{
+		#region Converter methods
+		/// <summary>
+		/// Convert a ITuple to a Tuple3dc
+		/// </summary>
+		/// <returns>The tuple3dc.</returns>
+		/// <param name="tuple">Tuple.</param>
+		public static Tuple3dc ToTuple3dc(this ITuple tuple)
+		{
+			return new Tuple3dc(tuple);
+		}
+
+		/// <summary>
+		/// Convert a ITuple to a Tuple3ds
+		/// </summary>
+		/// <returns>The tuple3ds.</returns>
+		/// <param name="tuple">Tuple.</param>
+		public static Tuple3ds ToTuple3ds(this ITuple tuple)
+		{
+			return new Tuple3ds(tuple);
+		}
+		#endregion
+	}
+}
+
