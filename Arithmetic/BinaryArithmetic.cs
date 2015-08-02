fd6c738f6c51eea138c671b109ae87f3adc815e8
seeseekey <kontakt@seeseekey.net>
2015-08-02 21:42:14 +0200

    Add binary arithmetic methods.

---
+++ Arithmetic/BinaryArithmetic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Xevle.Maths.Arithmetic
+{
+	public static class BinaryArithmetic
+	{
+		public static uint GetPowerOf2(uint size)
+		{
+			uint potenceOf2Size=1;
+			while(potenceOf2Size<size) potenceOf2Size*=2;
+			if(potenceOf2Size>size) potenceOf2Size/=2;
+			return potenceOf2Size;
+		}
+	}
+}
+
